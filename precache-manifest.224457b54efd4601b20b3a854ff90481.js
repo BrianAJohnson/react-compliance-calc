3be7654b516ebcd7f08abe92767d5f8c46118a8e
Brian Johnson <brirebmad@live.com>
2020-02-27 19:08:10 -0800

    Updates

---
+++ precache-manifest.224457b54efd4601b20b3a854ff90481.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549133464aae1a9e6a860657261ad522",
+    "url": "/react-compliance-calc/index.html"
+  },
+  {
+    "revision": "32e0d5b7632d88e2a58c",
+    "url": "/react-compliance-calc/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "42577925cd85146fb056",
+    "url": "/react-compliance-calc/static/js/2.9469e090.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-compliance-calc/static/js/2.9469e090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32e0d5b7632d88e2a58c",
+    "url": "/react-compliance-calc/static/js/main.d698fa54.chunk.js"
+  },
+  {
+    "revision": "6930bb01f82d9824513e",
+    "url": "/react-compliance-calc/static/js/runtime-main.0c5c0347.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-compliance-calc/static/media/logo.5d5d9eef.svg"
+  }
+]);
